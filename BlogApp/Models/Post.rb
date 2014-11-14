973c15c8f4688225b4098f5983ae1f008c478164
JArmando <JArmando@racowireless.com>
2014-11-14 09:40:32 -0400

    Creating Post class

---
+++ BlogApp/Models/Post.rb
@@ -0,0 +1,18 @@
+class Post
+	def initialize(author, content, tags)
+		@Author = author
+		@Content = content
+		@Tags = tags
+	end
+
+	def Author
+		@Author
+	end
+	def Content
+		@Content
+	end
+	def Tags
+		@Tags
+	end
+
+end
