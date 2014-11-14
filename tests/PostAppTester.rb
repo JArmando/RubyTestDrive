b0f21e5f1a692969093c28bf2cc6d4b00afa8666
JArmando <JArmando@racowireless.com>
2014-11-14 16:33:09 -0400

    created test for instance of Post

--- tests/PostAppTester.rb
+++ tests/PostAppTester.rb
@@ -3,11 +3,15 @@ require "test/unit"
 
 class TestPostReader < Test::Unit::TestCase
 	def testPostClass
+
+		assert_instance_of(Post, @testPost, "Should be able to create a new Post")
+		
 		@testPost = Post.new('Pablo', 'Batida se mato')
 		assert_equal('Pablo', @testPost.author, "Should be able to create a new Post with the given Author name")
 		
 		@testPost.tags = 'batida, pablo, muerte'
 		assert_equal('batida, pablo, muerte', @testPost.tags, "Should be able to set tags to a post")
+
 	end
 
 end
