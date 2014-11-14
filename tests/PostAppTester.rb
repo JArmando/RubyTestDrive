d8c59e70bdd90f07e68910a73898b38ac18e6ff5
JArmando <JArmando@racowireless.com>
2014-11-14 17:15:27 -0400

    fixing error on Post tests

--- tests/PostAppTester.rb
+++ tests/PostAppTester.rb
@@ -3,10 +3,10 @@ require "test/unit"
 
 class TestPostReader < Test::Unit::TestCase
 	def testPostClass
+		@testPost = Post.new('Pablo', 'Batida se mato')
 
 		assert_instance_of(Post, @testPost, "Should be able to create a new Post")
 		
-		@testPost = Post.new('Pablo', 'Batida se mato')
 		assert_equal('Pablo', @testPost.author, "Should be able to create a new Post with the given Author name")
 		
 		@testPost.tags = 'batida, pablo, muerte'
