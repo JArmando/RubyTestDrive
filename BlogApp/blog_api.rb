4184be91c0dcbeb291f779c46ea3e81ced94c15a
Jorge Armando <lobonegro11@gmail.com>
2014-11-15 11:31:05 -0400

    the api is able to return posts

--- BlogApp/blog_api.rb
+++ BlogApp/blog_api.rb
@@ -1,8 +1,11 @@
 require_relative './Helpers/post_reader'
 
 require 'sinatra'
+require 'json'
 
 get '/post' do
-	posts = PostReader.new().get_posts_from_file '/posts.json'
-	"Hello World"
+	content_type :json
+	posts = PostReader.new().get_posts_from_file('..\posts.json')
+	posts.map { |post| post.to_hash }.to_json
 end
+
