e0986691f7e41c0b3964a31206a804fe9432273a
Jorge Armando <lobonegro11@gmail.com>
2014-11-15 10:10:58 -0400

    Created to_hash Post's method

--- BlogApp/Models/Post.rb
+++ BlogApp/Models/Post.rb
@@ -7,4 +7,14 @@ class Post
 	attr_reader :author
 	attr_accessor :tags, :title, :description, :id
 
+	def to_hash
+		{
+      'id' => @id,
+      'author' => @author,
+      'title' => @title,
+      'description' => @description,
+      'tags' => @tags
+    }
+	end
+
 end
