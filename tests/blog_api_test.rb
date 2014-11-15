83ab1ca2154a681287a511bd4de987a2b85d460b
Jorge Armando <lobonegro11@gmail.com>
2014-11-14 23:09:45 -0400

    Created blog api test class

---
+++ tests/blog_api_test.rb
@@ -0,0 +1,19 @@
+require_relative '../BlogApp/blog_api'
+require 'sinatra'
+require 'test/unit'
+require 'rack/test'
+
+
+
+class TestBlogApi < Test::Unit::TestCase
+	include Rack::Test::Methods
+
+	def app
+		Sinatra::Application
+	end
+
+	def test_endpoint_exists
+		get '/post'
+		assert last_response.ok?
+	end
+end
