e7781a127e90660320964915d27966a2546ca4c0
Jorge Armando <lobonegro11@gmail.com>
2014-11-16 00:50:27 -0400

    Created more tests for the blog api

--- tests/specs/blog_api_spec.rb
+++ tests/specs/blog_api_spec.rb
@@ -10,10 +10,19 @@ describe 'The Great Blog Application' do
 		Sinatra::Application
 	end
 
-	it "works..." do
+	it "Works..." do
 		get '/post'
-		expect(last_response).to be_ok
+		last_response.status.must_equal 200
 	end
 
+	it "Can find a post by id" do
+		get '/post/1'
+		last_response.body.must_equal '{"id":"1","author":"Pixelator","title":null,"description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare sapien et mauris euismod, quis aliquet ante laoreet..","tags":"cooking, sports"}'
+	end
+
+	it "Can find posts by author" do
+		get '/post?author=Pixelator'
+		last_response.body.must_equal ''
+	end
 
 end
