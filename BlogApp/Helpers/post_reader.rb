c383f41e2c6956e2a6afc6cf4ca3b8cd45ada0d9
Jorge Armando <lobonegro11@gmail.com>
2014-11-16 01:02:00 -0400

    added code to parse title and tags from json file

--- BlogApp/Helpers/post_reader.rb
+++ BlogApp/Helpers/post_reader.rb
@@ -7,6 +7,8 @@ class PostReader
 		raw_data = JSON.parse(File.read(filename))
 		posts = raw_data['posts'].map {  |p| post = Post.new(p['author'], p['title'],p['description'])
 											 									 post.id = p['id']
+											 									 post.title = p['title']
+											 									 post.tags = p['tags'].split(',')
 											 									 post 
 									  							} 
 	end
