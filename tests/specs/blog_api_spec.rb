343ac5b02f6f511da0bd7de1721d27fa34e8ae53
Jorge Armando <lobonegro11@gmail.com>
2014-11-16 01:11:31 -0400

    refactoring api spec

--- tests/specs/blog_api_spec.rb
+++ tests/specs/blog_api_spec.rb
@@ -6,6 +6,10 @@ require 'minitest/autorun'
 describe 'The Great Blog Application' do
 	include Rack::Test::Methods
 
+pixalator_post = '{"id":"1","author":"Pixelator","title":"My first post","description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare sapien et mauris euismod, quis aliquet ante laoreet..","tags":["cooking"," sports"]}'
+pixelated_posts = '[{"id":"2","author":"Pixelated","title":"Your second post","description":"Morbi est sem, lacinia sit amet justo quis, maximus ullamcorper elit. Praesent in dolor nec sem rhoncus tincidunt nec egestas est.","tags":["fitness"," recreation"]},{"id":"3","author":"Pixelated","title":"Your third post","description":"Praesent in dolor nec sem rhoncus tincidunt nec egestas est.","tags":["fitness"]}]'
+
+
 	def app
 		Sinatra::Application
 	end
@@ -17,12 +21,12 @@ describe 'The Great Blog Application' do
 
 	it "Can find a post by id" do
 		get '/post/1'
-		last_response.body.must_equal '{"id":"1","author":"Pixelator","title":null,"description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare sapien et mauris euismod, quis aliquet ante laoreet..","tags":"cooking, sports"}'
+		last_response.body.must_equal pixalator_post
 	end
 
 	it "Can find posts by author" do
-		get '/post?author=Pixelator'
-		last_response.body.must_equal ''
+		get '/post?author=Pixelated'
+		last_response.body.must_equal pixelated_posts
 	end
 
 end
